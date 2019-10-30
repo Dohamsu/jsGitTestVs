10c23fbe4e283091f8de005afc6dd6d765d1b714
jwonkim <rlawlsdnjswk@naver.com>
2019-10-30 17:19:05 +0900

    promise 에러 케이스

--- src/MAN/js/main.js
+++ src/MAN/js/main.js
@@ -5,5 +5,21 @@
  * @desc    메인화면 
  */
 
+ new Promise(function(resolve, reject){
+  
+    var input = Math.floor(Math.random()*10);
 
- console.log("호에엥");
+      if(input > 4){ 
+        resolve(input)
+      }
+    reject(new Error("에러당ㅇㅇ"));
+  })
+  .then(function(result) {
+    console.log(result); // 1
+    return result + 10;
+  })
+  .then(function(result) {
+    console.log(result); // 31
+  }).then().catch(function (err) {
+    console.log(err); // Error: Request is failed
+  });
